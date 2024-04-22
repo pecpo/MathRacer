f96f3aea58f41babdbd40c76cf95845ca1c88bf4
Manan Aggarwal <mananaggarwal123@gmail.com>
2024-04-22 21:42:42 +0530

    input of charachters

--- PISEndSemProject.ino
+++ PISEndSemProject.ino
@@ -6,10 +6,22 @@ char op[] = {'+', '-', 'x', '/'};
 
 int pot1 = A0;
 int button1 = 2;
+int back1 = 4;
 bool flag1 = true;
+
+bool solving1 = false, solving2 = false;
 
 int ans1;
 int ans2;
+
+int coord1 = 0;
+
+void changeCoord1(int num) {
+  while(num != 0) {
+    coord1++;
+    num /= 10;
+  }
+}
 
 void displayProblem(LiquidCrystal_I2C lcd, int &ans) {
   int r = random(4);
@@ -30,13 +42,30 @@ void displayProblem(LiquidCrystal_I2C lcd, int &ans) {
   lcd.clear();
 
   lcd.setCursor(0, 0);
-  
+
   lcd.print(num1);
+
+  changeCoord1(num1);
+
   lcd.print(" ");
+
+  changeCoord1(1);
+
   lcd.print(op[r]);
+
+  changeCoord1(1);
+
   lcd.print(" ");
+
+  changeCoord1(1);
+
   lcd.print(num2);
+
+  changeCoord1(num2);
+
   lcd.print(" = ");
+
+  changeCoord1(100);
 
   switch(r) {
     case 0:
@@ -60,32 +89,45 @@ void setup() {
   Serial.begin(9600);
   pinMode(pot1, INPUT);
   pinMode(button1, INPUT_PULLUP);
+  pinMode(back1, INPUT_PULLUP);
 
   lcd1.init();
   lcd1.backlight();
+  lcd1.cursor();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // int r = analogRead(pot1);
-  // int val = map(r, 0, 1023, 0, 9);
-  // Serial.println(val);
+  if (!solving1) {
+    displayProblem(lcd1, ans1);
+    solving1 = true;
+  }
 
-  // int k = digitalRead(button1);
+  int r = analogRead(pot1);
+  int val = map(r, 0, 1023, 0, 9);
+  Serial.println(val);
 
-  //   if (k == 0) {
-  //     if (flag1) {
-  //     Serial.print(map(analogRead(pot1), 0, 1023, 0, 9));
-  //     flag1 = false;
-  //     }
-  //   }
-  //   else flag1 = true;
+  int buttonVal1 = digitalRead(button1);
 
-  displayProblem(lcd1, ans1);
+  int num1 = map(analogRead(pot1), 0, 1023, 0, 10);
 
-  lcd1.print(ans1);
+  // lcd1.blink();
 
-  delay(5000);
+  lcd1.print(num1);
+  lcd1.setCursor(coord1, 0);
+
+  if (buttonVal1 == 0) {
+    if (flag1) {
+      coord1++;
+      flag1 = false;
+    }
+  }
+  else flag1 = true;
+
+  Serial.println(digitalRead(button1));
+
+
+  delay(250);
 
 }
